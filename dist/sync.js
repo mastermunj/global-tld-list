a81d0fd56eeef5c940496eb7ce6d0a189b429e83
Munjal Dhamecha <mastermunj@gmail.com>
2020-06-23 16:55:38 +0530

    build(deps-dev): bump various eslint packages

--- dist/sync.js
+++ dist/sync.js
@@ -18,57 +18,54 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const punycode_1 = require("punycode");
 // import { TLDs as oldTLDs } from './index';
-let Sync = /** @class */ (() => {
-    class Sync {
-        static srcPath() {
-            return path_1.default.dirname(__dirname) + '/src';
-        }
-        static getData() {
-            return __awaiter(this, void 0, void 0, function* () {
-                return axios_1.default.get(Sync.ianaUrl);
-            });
-        }
-        static process(data) {
-            const tlds = [];
-            data
-                .toLowerCase()
-                .split('\n')
-                .forEach((line) => {
-                if (line.length <= 0 || line.startsWith('#')) {
-                    return;
-                }
-                if (line.startsWith('xn--')) {
-                    line = punycode_1.toUnicode(line);
-                }
-                tlds.push(line.trim());
-            });
-            return tlds;
-        }
-        static difference(oldTLDs, newTLDs) {
-            return {
-                added: newTLDs.filter((tld) => !oldTLDs.includes(tld)),
-                removed: oldTLDs.filter((tld) => !newTLDs.includes(tld)),
-            };
-        }
-        static exportableTLDs(tlds) {
-            const data = JSON.stringify(tlds, null, 2)
-                .replace(/"/gi, `'`)
-                .replace(`\n]`, `,\n]`);
-            return `export const TLDs = ${data};\n`;
-        }
-        static writeTLDs(tlds) {
-            const data = Sync.exportableTLDs(tlds);
-            fs_1.default.writeFileSync(`${Sync.srcPath()}/index.ts`, data);
-        }
-        static do() {
-            return __awaiter(this, void 0, void 0, function* () {
-                const response = yield Sync.getData();
-                const tlds = Sync.process(response.data);
-                Sync.writeTLDs(tlds);
-            });
-        }
+class Sync {
+    static srcPath() {
+        return path_1.default.dirname(__dirname) + '/src';
     }
-    Sync.ianaUrl = 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt';
-    return Sync;
-})();
+    static getData() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return axios_1.default.get(Sync.ianaUrl);
+        });
+    }
+    static process(data) {
+        const tlds = [];
+        data
+            .toLowerCase()
+            .split('\n')
+            .forEach((line) => {
+            if (line.length <= 0 || line.startsWith('#')) {
+                return;
+            }
+            if (line.startsWith('xn--')) {
+                line = punycode_1.toUnicode(line);
+            }
+            tlds.push(line.trim());
+        });
+        return tlds;
+    }
+    static difference(oldTLDs, newTLDs) {
+        return {
+            added: newTLDs.filter((tld) => !oldTLDs.includes(tld)),
+            removed: oldTLDs.filter((tld) => !newTLDs.includes(tld)),
+        };
+    }
+    static exportableTLDs(tlds) {
+        const data = JSON.stringify(tlds, null, 2)
+            .replace(/"/gi, `'`)
+            .replace(`\n]`, `,\n]`);
+        return `export const TLDs = ${data};\n`;
+    }
+    static writeTLDs(tlds) {
+        const data = Sync.exportableTLDs(tlds);
+        fs_1.default.writeFileSync(`${Sync.srcPath()}/index.ts`, data);
+    }
+    static do() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield Sync.getData();
+            const tlds = Sync.process(response.data);
+            Sync.writeTLDs(tlds);
+        });
+    }
+}
 exports.Sync = Sync;
+Sync.ianaUrl = 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt';
